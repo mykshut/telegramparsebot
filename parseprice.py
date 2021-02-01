8d3ba23b2cce4f3711c94313008c686abd76332e
MykShut <57766485+mykshut@users.noreply.github.com>
2021-02-01 16:56:37 +0100

    Add files via upload
    add csv process and one new shop.

--- parseprice.py
+++ parseprice.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import pandas as pd
+import csv
+
 
 all_titles = []
 all_black_prices = []
@@ -32,6 +34,17 @@ def identify_shop(URL, soup): #2
                 print(f'We found {scopes} in your {URL} so we think your shop is {scopes.upper()}')
                 time.sleep(1)
                 mediamarkt(soup)
+
+            if scopes == 'coffeeproficiency':
+                print(f'We found {scopes} in your {URL} so we think your shop is {scopes.upper()}')
+                time.sleep(1)
+                coffeeproficiency(soup)
+
+            if scopes == 'mediaexpert':
+                print(f'We found {scopes} in your {URL} so we think your shop is {scopes.upper()}')
+                time.sleep(1)
+                mediaexpert(soup)
+
 
 def zalando(soup): #3.1
     title = soup.find_all('h1')
@@ -88,9 +101,45 @@ def mediamarkt(soup): #3.3
     black_price = soup.find('div', class_='m-priceBox_old')
     red_price = [i for i in str(red_price).split()][3]
     black_price = [i for i in str(black_price).split()][2]
+
     for i in title:
         title = i.get_text()
+
     promptcheck(title, black_price, red_price)
+
+def mediaexpert(soup):
+    title = soup.find_all('h1', class_='a-typo is-primary')
+    red_price = soup.find_all('span', class_='a-price_price')[0]
+    black_price = soup.find_all('span', class_='a-price_price')[2]
+
+    for i in title:
+        title = i.get_text()
+        for i in title.split('\n')[0:2]:
+            title = i
+
+    for i in red_price:
+        red_price = i
+
+    for i in black_price:
+        black_price = i
+    if red_price == black_price:
+        red_price = []
+
+    promptcheck(title, black_price, red_price)
+
+
+
+# def coffeeproficiency(soup):
+#     title = soup.find_all('h1', class_="product_title entry-title")
+#     black_price = soup.find_all('span', class_="woocommerce-Price-amount amount")
+#     for i in title:
+#         title = i.get_text()
+#     for i in black_price:
+#         black_price = i.get_text()
+#         black_price = black_price.split()[0]
+#         black_price = float(black_price.replace(',','.'))
+#     print(black_price)
+
 
 def promptcheck(title, black_price, red_price): #4
     # global all_titles, all_black_prices, all_red_prices
@@ -100,13 +149,13 @@ def promptcheck(title, black_price, red_price): #4
         print(f'Name of product: {title}')
         all_titles.append(title)
         all_black_prices.append(black_price)
-        all_red_prices.append('it`s not on sale')
+        all_red_prices.append('NOT FOR SALE')
     if red_price == []:
         print(f'Actuall Price: {black_price}')
         print(f'Name of product: {title}')
         all_titles.append(title)
         all_black_prices.append(black_price)
-        all_red_prices.append('it`s not on sale')
+        all_red_prices.append('NOT FOR SALE')
     else:
         print('THIS PRODUCT IS FOR SALE!')
         print(f'Name of product: {title}')
@@ -116,17 +165,26 @@ def promptcheck(title, black_price, red_price): #4
         all_black_prices.append(black_price)
         all_red_prices.append(red_price)
 
+
 def show_df(): #5
     for black,red,title in zip(all_black_prices, all_red_prices, all_titles):
-        print(f'Name of product: {title:>8} Black price:{black:<20} Red price:{red:<20}')
+        with open('products.csv', mode='a', encoding='utf-8') as prod_data:
+            prod_writer = csv.writer(prod_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            prod_writer.writerow([title, black, red])
+            prod_data.close()
+    print('product.csv file was succesfully updated with new data')
+
 
 def whileloopforlinks(): #0
     T = True
+    i = 1
     print('Write >> STOP << to exit program!')
     while T == True:
-        URL = input('Paste your URL here: ')
+        URL = input(f'{i}: Paste your URL here: ')
+
         if URL != 'STOP':
             bf_pre_process(URL)
+            i += 1
         if URL == 'STOP':
             time.sleep(1.5)
             print('Thank you!')
